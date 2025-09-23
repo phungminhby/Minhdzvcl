c73d71a2231215ec91a7dcd794f68991bac39705
Minh Phung <phungminhby@gmail.com>
2025-09-23 09:49:15 +0700

    a

---
+++ README.md
@@ -0,0 +1,2 @@
+# My First Website
+Trang demo học HTML/CSS/JS — project đầu tay.
---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+console.log("Hello from main.js");
+// ví dụ: hiển thị alert khi load
+alert('Chào anh yêu — bắt đầu học nào!');
