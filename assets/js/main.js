c30731b9202696bc617d6b9580c4d8dfb9f003da
Minh Phung <phungminhby@gmail.com>
2025-10-01 14:04:41 +0700

    html, css máy tính bỏ túi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 console.log("Hello from main.js");
-// ví dụ: hiển thị alert khi load
-// alert('Chào em yêu — xem thử trang web của anh nhé!');
+// lây ô hiển thị
+const display = document.getElementById("display"); //tạo biến display lấy giữ liệu từ id display
+const buttons = document.querySelectorAll('.button button'); //tạo biến buttons quét toàn bộ giữ liệu từ nút button trong class .button
+
+//lưu biến hiện tại dưới dạng chuỗi
+
+let current = '';
+
+//cập nhật hiện thị nếu rỗng hiện thị 0
+
+function updateDisplay{
+    display.value = current === '' ? '0' : current;
+}
